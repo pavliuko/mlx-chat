664fbfadbc06b01bdf0371d1dbaa1f2a8a509a71
pavliuko <pavliuk.aleksandr@gmail.com>
2025-06-12 16:17:45 +0100

    Initialize mlx-chat project with basic structure, including SwiftUI ContentView, app entry point, asset catalogs, and test files. Add .gitignore for build artifacts and user-specific files.

---
+++ mlx-chat/mlx_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  mlx_chatApp.swift
+//  mlx-chat
+//
+//  Created by pasashque on 12/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct mlx_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mlx-chatTests/mlx_chatTests.swift
@@ -0,0 +1,17 @@
+//
+//  mlx_chatTests.swift
+//  mlx-chatTests
+//
+//  Created by pasashque on 12/06/2025.
+//
+
+import Testing
+@testable import mlx_chat
+
+struct mlx_chatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
